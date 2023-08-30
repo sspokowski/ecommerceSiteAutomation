41f0028be97c291590d0256936a779e5b2aadc1d
sam <sam.spokowski@gmail.com>
2023-08-30 10:37:30 -0500

    readme

---
+++ readme.md
@@ -0,0 +1,23 @@
+# Welcome to my eCommerce Site Test Automation Suite!
+
+This is a sample project that includes some automated UI tests for the eCommerce site [Etsy](www.etsy.com).  It is written in TypeScript using the [WebdriverIO](www.webdriver.io) automation framework.  JavaScript is the language of the browser, so it is my preference to use it for web UI automation projects.  TypeScript was chosen for its type-safety and other benefits over vanilla JS.  WebDriverIO was chosen as the framework because it is incredibly easy to use, does not have a DSL, and with very little configuration runs on major browsers locally or in other environments.
+
+# Overview
+
+This project follows the popular Page Object Model for UI automation.  It treats individual web pages and components of pages (e.g., navigation bars, modal dialogues, etc) as programmatic objects that can be reused across the entire test suite.
+
+## Getting started
+
+Pre-requisites that need to be installed:
+- NodeJS
+- TypeScript
+- Yarn
+
+Install dependencies:
+`yarn`
+
+Create environment variables:
+`cp example.env .env`
+
+Run the tests:
+`yarn test`
--- test/specs/e2e/purchaseFlow.spec.ts
+++ test/specs/e2e/purchaseFlow.spec.ts
@@ -10,23 +10,28 @@ import Shipping from '../../pageobjects/shipping.page';
 
 describe('Etsy end-to-end purchase tests', () => {
     it('should be able search for a specific product, view product detail page, and checkout as a guest', async () => {
+        //Navigate to Etsy and search for desired item
         await LandingPage.open();
         await TopNav.inputSearch.setValue('Swarovski crystal pearl dragonfly gold brooch');
         await TopNav.btnSearch.click();
-
         await SearchResults.firstSearchResult.click();
         
-        //Product detail pages open in new tab, must wait for the new tab to open and then switch focus
+        //Product detail pages open in new tab that initializes with an 'about:blank' url,
+        //We must hardcode a pause for the tab to redirct otherwise an error is thrown to the test runner
         await browser.pause(2500);
         await browser.switchWindow(/https:\/\/www.etsy.com\/listing\/.*/);
+
+        //Add item to cart from product detail page and verify success message
         await expect(ProductDetailPage.btnAddToCart).toExist();
         await ProductDetailPage.btnAddToCart.click();
         await expect(ProductDetailPage.headerAddToCartSuccess).toHaveText('1 item added to cart');
 
+        //Go to cart and proceed to checkout as guest
         await Cart.open();
         await Cart.btnCheckout.click();
-        
         await CheckoutModal.btnContinueAsGuest.click();
+        
+        //Fill in shipping details and continue to payment
         await Shipping.fillRandomShippingData();
         await browser.scroll(0, 500);
         await Shipping.btnContinueToPayment.click();
