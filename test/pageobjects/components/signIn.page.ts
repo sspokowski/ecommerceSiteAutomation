38fb7348425b0c49121cb64989cce5e678959819
sam <sam.spokowski@gmail.com>
2023-08-30 18:09:29 -0500

    adding auth test

--- test/pageobjects/components/signIn.page.ts
+++ test/pageobjects/components/signIn.page.ts
@@ -8,7 +8,7 @@ class SignIn extends Page {
     /**
      * define selectors using getter methods
      */
-    public get inputUsername () { return $('input[name="email"]') };
+    public get inputEmail () { return $('input[name="email"]') };
 
     public get inputPassword () { return $('input[name="password"]') };
 
@@ -18,8 +18,8 @@ class SignIn extends Page {
      * a method to encapsule automation code to interact with the page
      * e.g. to login using username and password
      */
-    public async login (username: string, password?: string) {
-        await this.inputUsername.setValue(username);
+    public async login (email: string, password?: string) {
+        await this.inputEmail.setValue(email);
         await this.inputPassword.setValue(password || '');
         await this.btnSubmit.click();
     }
--- test/specs/e2e/purchaseFlow.spec.ts
+++ test/specs/e2e/purchaseFlow.spec.ts
@@ -6,6 +6,7 @@ import ProductDetailPage from '../../pageobjects/productDetailPage.page';
 import Cart from '../../pageobjects/cart.page';
 import CheckoutModal from '../../pageobjects/components/checkoutModal.page';
 import Shipping from '../../pageobjects/shipping.page';
+import SignIn from '../../pageobjects/components/signIn.page';
 
 describe('Etsy end-to-end purchase tests as Guest', () => {
     it('should be able search for a specific product, view product detail page, and checkout as a guest', async () => {
@@ -39,3 +40,38 @@ describe('Etsy end-to-end purchase tests as Guest', () => {
     });
 });
 
+describe('Etsy end-to-end purchase tests as authenticated user', () => {
+    it('should be able search for a specific product, view product detail page, and checkout as a guest', async () => {
+        //Navigate to Etsy and search for desired item
+        await LandingPage.open();
+        await TopNav.btnSignIn.click();
+
+        await SignIn.login('sam.spokowski@gmail.com', process.env.PASSWORD);
+
+        await TopNav.inputSearch.setValue('Swarovski crystal pearl dragonfly gold brooch');
+        await TopNav.btnSearch.click();
+        await SearchResults.firstSearchResult.click();
+        
+        //Product detail pages open in new tab that initializes with an 'about:blank' url,
+        //We must hardcode a pause for the tab to redirct otherwise an error is thrown to the test runner
+        await browser.pause(2500);
+        await browser.switchWindow(/https:\/\/www.etsy.com\/listing\/.*/);
+
+        //Add item to cart from product detail page and verify success message
+        await expect(ProductDetailPage.btnAddToCart).toExist();
+        await ProductDetailPage.btnAddToCart.click();
+        await expect(ProductDetailPage.headerAddToCartSuccess).toHaveText('1 item added to cart');
+
+        //Go to cart and proceed to checkout as guest
+        await Cart.open();
+        await Cart.btnCheckout.click();
+        await CheckoutModal.btnContinueAsGuest.click();
+        
+        //Fill in shipping details and continue to payment
+        await Shipping.fillRandomShippingData();
+        await browser.scroll(0, 500);
+        await Shipping.btnContinueToPayment.click();
+
+        //Payment information would be entered on the next screens, but the test will end here for demonstration purposes.
+    });
+});
